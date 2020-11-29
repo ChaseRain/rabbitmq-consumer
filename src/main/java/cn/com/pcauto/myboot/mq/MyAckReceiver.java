2b2dd143e94e33624a18123b18897de2f3bc3dca
linlixin <linlixin@pconline.com.cn>
2020-11-29 18:25:06 +0800

    多个队列也变成手动确认且处理业务逻辑

--- src/main/java/cn/com/pcauto/myboot/mq/MyAckReceiver.java
+++ src/main/java/cn/com/pcauto/myboot/mq/MyAckReceiver.java
@@ -21,10 +21,23 @@ public class MyAckReceiver implements ChannelAwareMessageListener {
             String messageId=msgMap.get("messageId");
             String messageData=msgMap.get("messageData");
             String createTime=msgMap.get("createTime");
-            System.out.println("  MyAckReceiver  messageId:"+messageId+"  messageData:"+messageData+"  createTime:"+createTime);
-            System.out.println("消费的主题消息来自："+message.getMessageProperties().getConsumerQueue());
-            channel.basicAck(deliveryTag, true); //第二个参数，手动确认可以被批处理，当该参数为 true 时，则可以一次性确认 delivery_tag 小于等于传入值的所有消息
-//			channel.basicReject(deliveryTag, true);//第二个参数，true会重新放回队列，所以需要自己根据业务逻辑判断什么时候使用拒绝
+
+            if ("TestDirectQueue".equals(message.getMessageProperties().getConsumerQueue())){
+                System.out.println("消费的消息来自的队列名为："+message.getMessageProperties().getConsumerQueue());
+                System.out.println("消息成功消费到  messageId:"+messageId+"  messageData:"+messageData+"  createTime:"+createTime);
+                System.out.println("执行TestDirectQueue中的消息的业务处理流程......");
+
+            }
+
+            if ("fanout.A".equals(message.getMessageProperties().getConsumerQueue())){
+                System.out.println("消费的消息来自的队列名为："+message.getMessageProperties().getConsumerQueue());
+                System.out.println("消息成功消费到  messageId:"+messageId+"  messageData:"+messageData+"  createTime:"+createTime);
+                System.out.println("执行fanout.A中的消息的业务处理流程......");
+
+            }
+
+            channel.basicAck(deliveryTag, true);
+//			channel.basicReject(deliveryTag, true);//为true会重新放回队列
         } catch (Exception e) {
             channel.basicReject(deliveryTag, false);
             e.printStackTrace();
